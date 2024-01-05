00a54d24e43097c13875cc0172654f48f21a7197
Coty Sanchez <constanzasanchezdigital@gmail.com>
2024-01-05 11:30:06 -0300

    modificando ProductManager

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ProductManager = require('./controllers/product-manager.js');
+const ProductManager = require('./product-manager.js');
 
 const app = express();
 const PORT = 8080;
