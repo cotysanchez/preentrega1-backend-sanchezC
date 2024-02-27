02a69260c1a773975536fecf25f1ef6450d98d04
Coty Sanchez <constanzasanchezdigital@gmail.com>
2024-02-27 15:54:54 -0300

    modificando admin

--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -4,7 +4,56 @@ const UserModel= require ("../dao/models/user.model.js");
 const {isValidPassword} = require("../utils/hashBcrypt.js");
 const passport =require ("passport");
 
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
 
+  
+  if (email === 'adminCoder@coder.com' && password === 'adminCod3r123') {
+    
+    req.session.user = {
+      first_name: 'Admin',
+      last_name: 'Admin',
+      email: email,
+      role: 'admin',
+    };
+    req.session.login = true;
+    return res.redirect('/products'); 
+  } else {
+    
+    const user = await UserModel.findOne({ email: email });
+
+    if (!user) {
+      
+      return res
+        .status(400)
+        .send({ status: 'error', message: 'Usuario no encontrado' });
+    }
+
+   
+    const isValid = await isValidPassword(password, user);
+    if (!isValid) {
+      
+      return res
+        .status(400)
+        .send({ status: 'error', message: 'Credenciales Inválidas' });
+    }
+
+   
+    req.session.user = {
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      role: user.role,
+    };
+    req.session.login = true;
+    return res.redirect('/products'); 
+  }
+});
+
+
+
+
+/*
 //Login - POST
 router.post("/login", passport.authenticate( "login", {failureRedirect:"api/sessions/faillogin"}), async (req,res)=>{
     if (!req.user) return res.status(400).send ({status: "error", message:"Credenciales Invalidas"});
@@ -30,6 +79,7 @@ router.post("/login", passport.authenticate( "login", {failureRedirect:"api/sess
     res.redirect("/products");
    
 });
+*/
 
 //Logout - GET
 router.get("/logout", (req,res)=>{
@@ -45,6 +95,7 @@ router.get("/logout", (req,res)=>{
     
 router.get("faillogin", async (req,res)=>{
     res.json({message: "fallo la estartegia"});
+    
 });
 
 
